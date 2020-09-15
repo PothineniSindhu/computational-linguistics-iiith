8d510f455649d2ba1650c8c3ce151e624865ea1b
sindhu <pothinenisindhu999@gmail.com>
2020-09-15 17:12:03 +0530

    3-2

--- RDIP/Codes/si.js
+++ RDIP/Codes/si.js
@@ -1,115 +1,24 @@
-var A1 =["John ate an apple before afternoon",
-"some students like to study in the night",
-"John and Mary went to church",
-"John went to church after eating",
-"did he go to market",
-"the woman who called my sister sells cosmetics",
-"John goes to the library and studies",
-"John ate an apple so did she",
-"the teacher returned the book after she noticed the error",
-"I told her that I bought a book yesterday"]
+var engcorp=[["John ate an apple before afternoon","before afternoon John ate an apple","John before afternoon ate an apple"],
+            ["some students like to study in the night","at night some students like to study"],
+            ["John and Mary went to church","Mary and John went to church"],
+            ["John went to church after eating","after eating John went to church","John after eating went to church"],
+            ["did he go to market","he did go to market"],
+            ["the woman who called my sister sells cosmetics","the woman who sells cosmetics called my sister","my sister who sells cosmetics called the woman","my sister who called the woman sells cosmetics"],
+            ["John goes to the library and studies","John studies and goes to the library"],
+            ["John ate an apple so did she","she ate an apple so did John"],
+            ["the teacher returned the book after she noticed the error","the teacher noticed the error after she returned the book","after the teacher returned the book she noticed the error","after the teacher noticed the error she returned the book","she returned the book after the teacher noticed the error","she noticed the error after the teacher returned the book","after she returned the book the teacher noticed the error","after she noticed the error the teacher returned the book"],
+            ["I told her that I bought a book yesterday","I told her yesterday that I bought a book","yesterday I told her that I bought a book","I bought a book that I told her yesterday","I bought a book yesterday that I told her","yesterday I bought a book that I told her"]
+        ];
 
-
-var B1=[["John ate an apple before afternoon",
-"before afternoon John ate an apple",
-"John before afternoon ate an apple"],
-
-["some students like to study in the night",
-"at night some students like to study"],
-
-["John and Mary went to church",
-"Mary and John went to church"],
-
-["John went to church after eating",
-"after eating John went to church",
-"John after eating went to church"],
-["did he go to market",
-"he did go to market"],
-
-["the woman who called my sister sells cosmetics",
-"the woman who sells cosmetics called my sister",
-"my sister who sells cosmetics called the woman",
-"my sister who called the woman sells cosmetics"],
-
-["John goes to the library and studies",
-"John studies and goes to the library"],
-
-["John ate an apple so did she",
-"she ate an apple so did John"],
-
-["the teacher returned the book after she noticed the error",
-"the teacher noticed the error after she returned the book",
-"after the teacher returned the book she noticed the error",
-"after the teacher noticed the error she returned the book",
-"she returned the book after the teacher noticed the error",
-"she noticed the error after the teacher returned the book",
-"after she returned the book the teacher noticed the error",
-"after she noticed the error the teacher returned the book"],
-
-["I told her that I bought a book yesterday",
-"I told her yesterday that I bought a book",
-"yesterday I told her that I bought a book",
-"I bought a book that I told her yesterday",
-"I bought a book yesterday that I told her"	,
-"yesterday I bought a book that I told her"]]
-
-var A2=["राम और श्याम बाजार गयें",
-"राम सोया और श्याम भी",
-"मैंने उसे बताया कि राम सो रहा है",
-"राम खाकर सोया",
-"बिल्लियों को मारकर कुत्ता सो गया",
-"एक लाल किताब वहाँ है",
-"एक बड़ी सी किताब वहाँ है"]
-
-var B2=[["राम और श्याम बाजार गयें",
-"राम और श्याम गयें बाजार",
-"बाजार गयें राम और श्याम",
-"गयें बाजार राम और श्याम"],
-
-["राम सोया और श्याम भी",
-"श्याम सोया और राम भी",
-"सोया श्याम और राम भी",
-"सोया राम और श्याम भी"],
-
-["मैंने उसे बताया कि राम सो रहा है",
-"मैंने उसे बताया कि सो रहा है राम",
-"उसे मैंने बताया कि राम सो रहा है",
-"उसे मैंने बताया कि सो रहा है राम",
-"मैंने बताया उसे कि राम सो रहा है",
-"मैंने बताया उसे कि सो रहा है राम",
-"उसे बताया मैंने कि राम सो रहा है",
-"उसे बताया मैंने कि सो रहा है राम",
-"बताया मैंने उसे कि राम सो रहा है",
-"बताया मैंने उसे कि सो रहा है राम",
-"बताया उसे मैंने कि राम सो रहा है",
-"बताया उसे मैंने कि सो रहा है राम"],
-
-["राम खाकर सोया","खाकर राम सोया",
-"राम सोया खाकर","खाकर सोया राम",
-"सोया राम खाकर","सोया खाकर राम"],
-
-["बिल्लियों को मारकर कुत्ता सो गया",
-"मारकर बिल्लियों को कुत्ता सो गया",
-"बिल्लियों को मारकर सो गया कुत्ता",
-"मारकर बिल्लियों को सो गया कुत्ता",
-"कुत्ता सो गया बिल्लियों को मारकर",
-"कुत्ता सो गया मारकर बिल्लियों को",
-"सो गया कुत्ता बिल्लियों को मारकर",
-"सो गया कुत्ता मारकर बिल्लियों को"],
-
-["एक लाल किताब वहाँ है",
-"एक लाल किताब है वहाँ",
-"वहाँ है एक लाल किताब",
-"है वहाँ एक लाल किताब"],
-
-["एक बड़ी सी किताब वहाँ है",
-"एक बड़ी सी किताब है वहाँ",
-"बड़ी सी एक किताब वहाँ है",
-"बड़ी सी एक किताब है वहाँ",
-"वहाँ है एक बड़ी सी किताब",
-"वहाँ है बड़ी सी एक किताब",
-"है वहाँ एक बड़ी सी किताब",
-"है वहाँ बड़ी सी एक किताब"]];
+var hincorp=[["à¤°à¤¾à¤® à¤”à¤° à¤¶à¥à¤¯à¤¾à¤® à¤¬à¤¾à¤œà¤¾à¤° à¤—à¤¯à¥‡à¤‚","à¤°à¤¾à¤® à¤”à¤° à¤¶à¥à¤¯à¤¾à¤® à¤—à¤¯à¥‡à¤‚ à¤¬à¤¾à¤œà¤¾à¤°","à¤¬à¤¾à¤œà¤¾à¤° à¤—à¤¯à¥‡à¤‚ à¤°à¤¾à¤® à¤”à¤° à¤¶à¥à¤¯à¤¾à¤®","à¤—à¤¯à¥‡à¤‚ à¤¬à¤¾à¤œà¤¾à¤° à¤°à¤¾à¤® à¤”à¤° à¤¶à¥à¤¯à¤¾à¤®"],
+            ["à¤°à¤¾à¤® à¤¸à¥‹à¤¯à¤¾ à¤”à¤° à¤¶à¥à¤¯à¤¾à¤® à¤­à¥€","à¤¶à¥à¤¯à¤¾à¤® à¤¸à¥‹à¤¯à¤¾ à¤”à¤° à¤°à¤¾à¤® à¤­à¥€","à¤¸à¥‹à¤¯à¤¾ à¤¶à¥à¤¯à¤¾à¤® à¤”à¤° à¤°à¤¾à¤® à¤­à¥€","à¤¸à¥‹à¤¯à¤¾ à¤°à¤¾à¤® à¤”à¤° à¤¶à¥à¤¯à¤¾à¤® à¤­à¥€"],
+            ["à¤®à¥ˆà¤‚à¤¨à¥‡ à¤‰à¤¸à¥‡ à¤¬à¤¤à¤¾à¤¯à¤¾ à¤•à¤¿ à¤°à¤¾à¤® à¤¸à¥‹ à¤°à¤¹à¤¾ à¤¹à¥ˆ","à¤®à¥ˆà¤‚à¤¨à¥‡ à¤‰à¤¸à¥‡ à¤¬à¤¤à¤¾à¤¯à¤¾ à¤•à¤¿ à¤¸à¥‹ à¤°à¤¹à¤¾ à¤¹à¥ˆ à¤°à¤¾à¤®","à¤‰à¤¸à¥‡ à¤®à¥ˆà¤‚à¤¨à¥‡ à¤¬à¤¤à¤¾à¤¯à¤¾ à¤•à¤¿ à¤°à¤¾à¤® à¤¸à¥‹ à¤°à¤¹à¤¾ à¤¹à¥ˆ","à¤‰à¤¸à¥‡ à¤®à¥ˆà¤‚à¤¨à¥‡ à¤¬à¤¤à¤¾à¤¯à¤¾ à¤•à¤¿ à¤¸à¥‹ à¤°à¤¹à¤¾ à¤¹à¥ˆ à¤°à¤¾à¤®","à¤®à¥ˆà¤‚à¤¨à¥‡ à¤¬à¤¤à¤¾à¤¯à¤¾ à¤‰à¤¸à¥‡ à¤•à¤¿ à¤°à¤¾à¤® à¤¸à¥‹ à¤°à¤¹à¤¾ à¤¹à¥ˆ","à¤®à¥ˆà¤‚à¤¨à¥‡ à¤¬à¤¤à¤¾à¤¯à¤¾ à¤‰à¤¸à¥‡ à¤•à¤¿ à¤¸à¥‹ à¤°à¤¹à¤¾ à¤¹à¥ˆ à¤°à¤¾à¤®","à¤‰à¤¸à¥‡ à¤¬à¤¤à¤¾à¤¯à¤¾ à¤®à¥ˆà¤‚à¤¨à¥‡ à¤•à¤¿ à¤°à¤¾à¤® à¤¸à¥‹ à¤°à¤¹à¤¾ à¤¹à¥ˆ","à¤‰à¤¸à¥‡ à¤¬à¤¤à¤¾à¤¯à¤¾ à¤®à¥ˆà¤‚à¤¨à¥‡ à¤•à¤¿ à¤¸à¥‹ à¤°à¤¹à¤¾ à¤¹à¥ˆ à¤°à¤¾à¤®","à¤¬à¤¤à¤¾à¤¯à¤¾ à¤®à¥ˆà¤‚à¤¨à¥‡ à¤‰à¤¸à¥‡ à¤•à¤¿ à¤°à¤¾à¤® à¤¸à¥‹ à¤°à¤¹à¤¾ à¤¹à¥ˆ","à¤¬à¤¤à¤¾à¤¯à¤¾ à¤®à¥ˆà¤‚à¤¨à¥‡ à¤‰à¤¸à¥‡ à¤•à¤¿ à¤¸à¥‹ à¤°à¤¹à¤¾ à¤¹à¥ˆ à¤°à¤¾à¤®","à¤¬à¤¤à¤¾à¤¯à¤¾ à¤‰à¤¸à¥‡ à¤®à¥ˆà¤‚à¤¨à¥‡ à¤•à¤¿ à¤°à¤¾à¤® à¤¸à¥‹ à¤°à¤¹à¤¾ à¤¹à¥ˆ","à¤¬à¤¤à¤¾à¤¯à¤¾ à¤‰à¤¸à¥‡ à¤®à¥ˆà¤‚à¤¨à¥‡ à¤•à¤¿ à¤¸à¥‹ à¤°à¤¹à¤¾ à¤¹à¥ˆ à¤°à¤¾à¤®"],
+            ["à¤°à¤¾à¤® à¤–à¤¾à¤•à¤° à¤¸à¥‹à¤¯à¤¾","à¤–à¤¾à¤•à¤° à¤°à¤¾à¤® à¤¸à¥‹à¤¯à¤¾","à¤°à¤¾à¤® à¤¸à¥‹à¤¯à¤¾ à¤–à¤¾à¤•à¤°","à¤–à¤¾à¤•à¤° à¤¸à¥‹à¤¯à¤¾ à¤°à¤¾à¤®","à¤¸à¥‹à¤¯à¤¾ à¤°à¤¾à¤® à¤–à¤¾à¤•à¤°","à¤¸à¥‹à¤¯à¤¾ à¤–à¤¾à¤•à¤° à¤°à¤¾à¤®"],
+            ["à¤•à¥à¤¯à¤¾ à¤µà¥‹ à¤¬à¤¾à¤œà¤¾à¤° à¤—à¤¯à¤¾","à¤µà¥‹ à¤•à¥à¤¯à¤¾ à¤¬à¤¾à¤œà¤¾à¤° à¤—à¤¯à¤¾","à¤•à¥à¤¯à¤¾ à¤¬à¤¾à¤œà¤¾à¤° à¤µà¥‹ à¤—à¤¯à¤¾","à¤µà¥‹ à¤¬à¤¾à¤œà¤¾à¤° à¤•à¥à¤¯à¤¾ à¤—à¤¯à¤¾","à¤¬à¤¾à¤œà¤¾à¤° à¤•à¥à¤¯à¤¾ à¤µà¥‹ à¤—à¤¯à¤¾","à¤¬à¤¾à¤œà¤¾à¤° à¤µà¥‹ à¤•à¥à¤¯à¤¾ à¤—à¤¯à¤¾","à¤•à¥à¤¯à¤¾ à¤µà¥‹ à¤—à¤¯à¤¾ à¤¬à¤¾à¤œà¤¾à¤°","à¤µà¥‹ à¤•à¥à¤¯à¤¾ à¤—à¤¯à¤¾ à¤¬à¤¾à¤œà¤¾à¤°","à¤•à¥à¤¯à¤¾ à¤—à¤¯à¤¾ à¤µà¥‹ à¤¬à¤¾à¤œà¤¾à¤°","à¤µà¥‹ à¤—à¤¯à¤¾ à¤•à¥à¤¯à¤¾ à¤¬à¤¾à¤œà¤¾à¤°","à¤—à¤¯à¤¾ à¤•à¥à¤¯à¤¾ à¤µà¥‹ à¤¬à¤¾à¤œà¤¾à¤°","à¤—à¤¯à¤¾ à¤µà¥‹ à¤•à¥à¤¯à¤¾ à¤¬à¤¾à¤œà¤¾à¤°","à¤—à¤¯à¤¾ à¤•à¥à¤¯à¤¾ à¤¬à¤¾à¤œà¤¾à¤° à¤µà¥‹","à¤•à¥à¤¯à¤¾ à¤—à¤¯à¤¾ à¤¬à¤¾à¤œà¤¾à¤° à¤µà¥‹","à¤—à¤¯à¤¾ à¤¬à¤¾à¤œà¤¾à¤° à¤•à¥à¤¯à¤¾ à¤µà¥‹","à¤•à¥à¤¯à¤¾ à¤¬à¤¾à¤œà¤¾à¤° à¤—à¤¯à¤¾ à¤µà¥‹","à¤¬à¤¾à¤œà¤¾à¤° à¤—à¤¯à¤¾ à¤•à¥à¤¯à¤¾ à¤µà¥‹","à¤¬à¤¾à¤œà¤¾à¤° à¤—à¤¯à¤¾ à¤•à¥à¤¯à¤¾ à¤µà¥‹","à¤µà¥‹ à¤¬à¤¾à¤œà¤¾à¤° à¤—à¤¯à¤¾ à¤•à¥à¤¯à¤¾","à¤¬à¤¾à¤œà¤¾à¤° à¤µà¥‹ à¤—à¤¯à¤¾ à¤•à¥à¤¯à¤¾","à¤¬à¤¾à¤œà¤¾à¤° à¤—à¤¯à¤¾ à¤µà¥‹ à¤•à¥à¤¯à¤¾","à¤µà¥‹ à¤—à¤¯à¤¾ à¤¬à¤¾à¤œà¤¾à¤° à¤•à¥à¤¯à¤¾","à¤—à¤¯à¤¾ à¤µà¥‹ à¤¬à¤¾à¤œà¤¾à¤° à¤•à¥à¤¯à¤¾","à¤—à¤¯à¤¾ à¤¬à¤¾à¤œà¤¾à¤° à¤µà¥‹ à¤•à¥à¤¯à¤¾"],
+            ["à¤¬à¤¿à¤²à¥à¤²à¤¿à¤¯à¥‹à¤‚ à¤•à¥‹ à¤®à¤¾à¤°à¤•à¤° à¤•à¥à¤¤à¥à¤¤à¤¾ à¤¸à¥‹ à¤—à¤¯à¤¾","à¤®à¤¾à¤°à¤•à¤° à¤¬à¤¿à¤²à¥à¤²à¤¿à¤¯à¥‹à¤‚ à¤•à¥‹ à¤•à¥à¤¤à¥à¤¤à¤¾ à¤¸à¥‹ à¤—à¤¯à¤¾","à¤¬à¤¿à¤²à¥à¤²à¤¿à¤¯à¥‹à¤‚ à¤•à¥‹ à¤®à¤¾à¤°à¤•à¤° à¤¸à¥‹ à¤—à¤¯à¤¾ à¤•à¥à¤¤à¥à¤¤à¤¾","à¤®à¤¾à¤°à¤•à¤° à¤¬à¤¿à¤²à¥à¤²à¤¿à¤¯à¥‹à¤‚ à¤•à¥‹ à¤¸à¥‹ à¤—à¤¯à¤¾ à¤•à¥à¤¤à¥à¤¤à¤¾","à¤•à¥à¤¤à¥à¤¤à¤¾ à¤¸à¥‹ à¤—à¤¯à¤¾ à¤¬à¤¿à¤²à¥à¤²à¤¿à¤¯à¥‹à¤‚ à¤•à¥‹ à¤®à¤¾à¤°à¤•à¤°","à¤•à¥à¤¤à¥à¤¤à¤¾ à¤¸à¥‹ à¤—à¤¯à¤¾ à¤®à¤¾à¤°à¤•à¤° à¤¬à¤¿à¤²à¥à¤²à¤¿à¤¯à¥‹à¤‚ à¤•à¥‹","à¤¸à¥‹ à¤—à¤¯à¤¾ à¤•à¥à¤¤à¥à¤¤à¤¾ à¤¬à¤¿à¤²à¥à¤²à¤¿à¤¯à¥‹à¤‚ à¤•à¥‹ à¤®à¤¾à¤°à¤•à¤°","à¤¸à¥‹ à¤—à¤¯à¤¾ à¤•à¥à¤¤à¥à¤¤à¤¾ à¤®à¤¾à¤°à¤•à¤° à¤¬à¤¿à¤²à¥à¤²à¤¿à¤¯à¥‹à¤‚ à¤•à¥‹"],
+            ["à¤à¤• à¤²à¤¾à¤² à¤•à¤¿à¤¤à¤¾à¤¬ à¤µà¤¹à¤¾à¤ à¤¹à¥ˆ","à¤à¤• à¤²à¤¾à¤² à¤•à¤¿à¤¤à¤¾à¤¬ à¤¹à¥ˆ à¤µà¤¹à¤¾à¤","à¤µà¤¹à¤¾à¤ à¤¹à¥ˆ à¤à¤• à¤²à¤¾à¤² à¤•à¤¿à¤¤à¤¾à¤¬","à¤¹à¥ˆ à¤µà¤¹à¤¾à¤ à¤à¤• à¤²à¤¾à¤² à¤•à¤¿à¤¤à¤¾à¤¬"],
+            ["à¤à¤• à¤¬à¥œà¥€ à¤¸à¥€ à¤•à¤¿à¤¤à¤¾à¤¬ à¤µà¤¹à¤¾à¤ à¤¹à¥ˆ","à¤à¤• à¤¬à¥œà¥€ à¤¸à¥€ à¤•à¤¿à¤¤à¤¾à¤¬ à¤¹à¥ˆ à¤µà¤¹à¤¾à¤","à¤¬à¥œà¥€ à¤¸à¥€ à¤à¤• à¤•à¤¿à¤¤à¤¾à¤¬ à¤µà¤¹à¤¾à¤ à¤¹à¥ˆ","à¤¬à¥œà¥€ à¤¸à¥€ à¤à¤• à¤•à¤¿à¤¤à¤¾à¤¬ à¤¹à¥ˆ à¤µà¤¹à¤¾à¤","à¤µà¤¹à¤¾à¤ à¤¹à¥ˆ à¤à¤• à¤¬à¥œà¥€ à¤¸à¥€ à¤•à¤¿à¤¤à¤¾à¤¬","à¤µà¤¹à¤¾à¤ à¤¹à¥ˆ à¤¬à¥œà¥€ à¤¸à¥€ à¤à¤• à¤•à¤¿à¤¤à¤¾à¤¬","à¤¹à¥ˆ à¤µà¤¹à¤¾à¤ à¤à¤• à¤¬à¥œà¥€ à¤¸à¥€ à¤•à¤¿à¤¤à¤¾à¤¬","à¤¹à¥ˆ à¤µà¤¹à¤¾à¤ à¤¬à¥œà¥€ à¤¸à¥€ à¤à¤• à¤•à¤¿à¤¤à¤¾à¤¬"]
+        ];
 function language(){
     
     document.getElementById("intro").innerHTML="<b>Form a sentence (Declarative or Interrogative or any other type) from the given words</b><br>(select the buttons in proper order)";
@@ -119,5 +28,33 @@ function language(){
     else{
         corpus=hincorp;
     }
+division=document.getElementById("w_buttons");
+    division.innerHTML="";
+    if(document.getElementById("lang").value=="english"){
+        corpus=engcorp;
+    }
+    else{
+        corpus=hincorp;
+    }
+    index=Math.floor(Math.random()*corpus.length);
+    sentence=corpus[index][0];
+    words=sentence.split(" ");
+    console.log(words);
+    for(i=0;i<words.length;i++){
+        ind=Math.floor(Math.random()*words.length);
+        temp=words[ind];
+        words[ind]=words[i];
+        words[i]=temp;
+    }
+    but=[]
+    for(i=0;i<words.length;i++){
+        but[i]=document.createElement("input");
+        but[i].type="button";
+        but[i].value=words[i];
+        but[i].style.display="inline";
+        but[i].style.margin="0px 5px";
+        division.appendChild(but[i]);
+    }
+    console.log(but);
 }
  
